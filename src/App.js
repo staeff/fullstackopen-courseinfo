133171b1090911234559289d834f04b5d378f887
Stephan Klinger <staeff@arcor.de>
2021-08-04 23:47:46 +0200

    Exercise 1.4: Make use of an array

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -23,31 +23,35 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  let count = props.parts.map(value => value.exercises).reduce((a, b) => a + b, 0)
+
   return (
-    <p>Number of exercises: {props.count}</p>
+    <p>Number of exercises: {count}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course} />
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
