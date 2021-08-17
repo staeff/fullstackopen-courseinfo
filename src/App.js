cde87626a058096d92fef0bb2373fd566ec6faaf
Stephan Klinger <staeff@arcor.de>
2021-08-17 17:17:11 +0200

    Add ids and use map in Part component

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {props.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
     </div>
   )
 }
@@ -32,19 +32,23 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
